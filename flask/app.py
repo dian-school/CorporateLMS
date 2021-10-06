f73fa3869d9364e64d9ec623d199a13ba95f3da9
dian-farah <80142534+dian-school@users.noreply.github.com>
2021-10-06 21:38:41 +0800

    edit lms_database

--- flask/app.py
+++ flask/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
-                                        '@localhost:3306/is212_example'
+                                        '@localhost:3306/lms_database'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                            'pool_recycle': 280}
@@ -20,8 +20,8 @@ class Courses(db.Model):
     course_title = db.Column(db.String(26))
     description = db.Column(db.String(1000))
     prerequisites = db.Column(db.String(1000))
-    class = db.Column(db.Integer)
-    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'))
+    class_section = db.Column(db.Integer)
+    class_size = db.Column(db.Integer, db.ForeignKey('patient.id'))
 
     def to_dict(self):
         """
