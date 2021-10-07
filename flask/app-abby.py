bfc1abe942c7265f106bb945417a741e49582157
abby-lee <abigail.lee.2019@sis.smu.edu.sg>
2021-10-07 15:53:54 +0800

    test commit

--- flask/app-abby.py
+++ flask/app-abby.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import *
 from flask_cors import CORS
 
+import json
+from os import environ
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
-                                        '@localhost:3306/lms_database'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/lms_database' or 'mysql+mysqlconnector://root:root@localhost:3306/lms_database'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
-                                           'pool_recycle': 280}
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
 db = SQLAlchemy(app)
 
@@ -32,6 +36,102 @@ class Courses(db.Model):
             result[column] = getattr(self, column)
         return result
     
+@app.route("/courses")
+def get_all():
+    course_list = Courses.query.all()
+    if len(course_list):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "courses": [course.json() for 
+                    course in course_list]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no courses."
+        }
+    ), 404
+
+@app.route("/courses/<string:course_code>")
+def find_by_course_code(course_code):
+    course = Courses.query.filter_by(course_code=course_code).first()
+    if course:
+        return jsonify(
+            {
+                "code": 200,
+                "data": course.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Course code not found."
+        }
+    ), 404
+
+@app.route("/courses/<string:course_title>")
+def find_by_course_title(course_title):
+    course = Courses.query.filter_by(course_title=course_title).first()
+    if course:
+        return jsonify(
+            {
+                "code": 200,
+                "data": course.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Course title not found."
+        }
+    ), 404
+
+@app.route("/courses", methods=['POST'])
+def add_course():
+    addCourseName = request.json.get('course_title', None)
+    addCode = request.json.get('course_code', None)
+    addDescription = request.json.get('description', None)
+    addPrerequisites = request.json.get('prerequisites', None)
+
+    #Checks if the current entry is already inside the course database: Checks by code, course Name, description
+    courseCheck = Courses.query.filter(Courses.course_code.like(addCode), Courses.course_title.like(addCourseName),Courses.description.like(addDescription),Courses.prerequisites.like(addPrerequisites)).first()
+
+    if (courseCheck):
+        return jsonify(
+            {
+                "code": 400,
+                "data": courseCheck.json(),
+                "message": "Course seems to be in the database already."
+            }
+        ), 400
+
+    data = request.get_json()
+    course = Courses(**data)
+
+    try:
+        db.session.add(course)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred while adding course. "
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": course.json()
+        }
+    ), 201
+
+
+
 class Sections(db.Model):
     __tablename__ = 'sections'
 
@@ -133,4 +233,8 @@ class Enrols(db.Model):
 
 db.create_all()
 
+if __name__ == '__main__':
+    print("This is flask for " + os.path.basename(__file__) +
+          ": lms G1T6 ...")
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
